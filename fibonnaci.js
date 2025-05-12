153482e0ae3c88b16631eeeaade4084679f72af3
Evan Ayalin <siyeel04@gmail.com>
2025-05-12 16:36:59 +0800

    Created fibonnaci sequence with recursion.

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,18 +1,40 @@
 function fibs(iteration) {
-    let prev= 0;
+    let prev = 0;
     let curr = 1;
     let next;
 
     const arr = [];
 
-    for(let index = 0; index < iteration; index++) {
+    for (let index = 0; index < iteration; index++) {
         arr.push(prev);
         next = curr + prev
-        prev= curr;
+        prev = curr;
         curr = next;
     };
 
     return arr;
 };
 
-console.log(fibs(8))
+function fibsRec(iteration) {
+    if (iteration <= 0) {
+        return [];
+    };
+
+    if (iteration === 1) {
+        return [0];
+    }
+
+    const arr = fibsRec(iteration - 1);
+
+    let prev = arr[arr.length - 2] || 0;
+    let curr = arr[arr.length - 1] || 1;
+    let next;
+
+    next = prev + curr;
+
+    arr.push(next);
+    return arr;
+};
+
+console.log(fibs(8));
+console.log(fibsRec(8));
